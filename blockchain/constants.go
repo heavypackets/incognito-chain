c0af0b40f700ff57271680999fc4077f40762662
hieu013 <ngochieu@autonomous.nyc>
2020-07-24 21:03:34 +0700

    Update Batch Size to optimize tps

--- blockchain/constants.go
+++ blockchain/constants.go
@@ -31,7 +31,7 @@ const (
 	NumberOfFixedBlockValidators  = 4
 	BEACON_ID                     = -1         // CommitteeID of beacon chain, used for highway
 	ValidateTimeForSpamRequestTxs = 1581565837 // GMT: Thursday, February 13, 2020 3:50:37 AM. From this time, block will be checked spam request-reward tx
-	TransactionBatchSize          = 30
+	TransactionBatchSize          = 40
 	SpareTime                     = 1000 // in mili-second
 )
 
