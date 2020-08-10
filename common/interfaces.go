a40483e984bffaa1fe5c0b274c084d645ea84892
0xKumi <dung.q@autonomous.nyc>
2020-08-10 14:28:53 +0700

    increase timeslot

--- common/interfaces.go
+++ common/interfaces.go
@@ -34,7 +34,7 @@ type ChainInterface interface {
 	GetShardID() int
 }
 
-const TIMESLOT = 10
+const TIMESLOT = 3
 
 func CalculateTimeSlot(time int64) int64 {
 	return int64(math.Floor(float64(time / TIMESLOT)))
